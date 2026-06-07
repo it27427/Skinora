2c2098e66b2509734d42bd9507cfc8f083281105
it2427 <istiak.tushar.qtec@gmail.com>
2026-06-08 00:07:23 +0600

    Modal Removed and Page Updated

--- assets/index.js
+++ assets/index.js
@@ -20,28 +20,46 @@ backToTop.addEventListener("click", () => {
   });
 });
 
+/** MULTI-LINKS-OPEN */
+document.getElementById("naturalCta").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const links = [
+    "https://www.naturalhealthsource.com/ct/984831",
+    "https://omg10.com/4/11088228",
+  ];
+
+  // open first immediately (guaranteed allowed)
+  window.open(links[0], "_blank");
+
+  // second open shortly after (still may be blocked in some browsers)
+  setTimeout(() => {
+    window.open(links[1], "_blank");
+  }, 500);
+});
+
 /** MODAL */
 
-const modalOverlay = document.getElementById("modalOverlay");
-const modalBox = document.getElementById("modalBox");
+// const modalOverlay = document.getElementById("modalOverlay");
+// const modalBox = document.getElementById("modalBox");
 
-const adLink =
-  "https://www.effectivecpmnetwork.com/w378h9jy2?key=498c8702c2927fdc52a06af79a4e5a1c";
+// const adLink =
+//   "https://www.effectivecpmnetwork.com/w378h9jy2?key=498c8702c2927fdc52a06af79a4e5a1c";
 
-// DOM loaded হলে modal show
-document.addEventListener("DOMContentLoaded", () => {
-  modalOverlay.classList.add("show");
-});
+// // DOM loaded হলে modal show
+// document.addEventListener("DOMContentLoaded", () => {
+//   modalOverlay.classList.add("show");
+// });
 
-// ad open + modal close
-modalBox.addEventListener("click", () => {
-  window.open(adLink, "_blank");
-  modalOverlay.classList.remove("show");
-});
+// // ad open + modal close
+// modalBox.addEventListener("click", () => {
+//   window.open(adLink, "_blank");
+//   modalOverlay.classList.remove("show");
+// });
 
-modalOverlay.addEventListener("click", (e) => {
-  if (e.target === modalOverlay) {
-    // intentionally empty (no close)
-    return;
-  }
-});
+// modalOverlay.addEventListener("click", (e) => {
+//   if (e.target === modalOverlay) {
+//     // intentionally empty (no close)
+//     return;
+//   }
+// });
